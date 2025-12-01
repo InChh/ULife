3efbac7d45902e6f64b1fcd3bb1e9d669bbbca2e
InChh <2319397152@qq.com>
2025-12-01 20:47:51 +0800

    chore: update project structure

---
+++ ULife/Screens/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  ULife
+//
+//  Created by 刘宏伟 on 2025/12/1.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ ULife/Utils/Consts.swift
@@ -0,0 +1,8 @@
+//
+//  Consts.swift
+//  ULife
+//
+//  Created by 刘宏伟 on 2025/12/1.
+//
+
+import Foundation
